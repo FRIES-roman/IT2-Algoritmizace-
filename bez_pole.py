15ef8601ce430881b16c291be16a864d96cfcc45
FRIES-roman <24302@student.vitkovickastredni.cz>
2025-10-14 11:35:24 +0200

    Add files via upload

--- bez_pole.py
+++ bez_pole.py
@@ -2,7 +2,7 @@ n = int(input("Zadej počet čísel: "))
 
 x = int(input("Zadej 1. číslo: "))
 Max = x
-pocet = 1
+vyskyt_max = 1
 prvni = 1
 posledni = 1
 
@@ -11,14 +11,14 @@ for i in range(2, n + 1):
 
     if x > Max:
         Max = x
-        pocet = 1
+        vyskyt_max = 1
         prvni = i
         posledni = i
     elif x == Max:
-        pocet += 1
+        vyskyt_max += 1
         posledni = i
 
 print(f"\nMaximum: {Max}")
 print(f"První výskyt: {prvni}. číslo v pořadí")
 print(f"Poslední výskyt: {posledni}. číslo v pořadí")
-print(f"Počet výskytů maxima: {pocet}")
+print(f"Počet výskytů maxima: {vyskyt_max}")
