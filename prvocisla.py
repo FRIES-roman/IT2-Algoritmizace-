455f9c8b6d1cf28620b1fdd1ee7cf3f824f6e59d
FRIES-roman <24302@student.vitkovickastredni.cz>
2025-10-15 12:05:56 +0200

    prvocisla.py

--- prvocisla.py
+++ prvocisla.py
@@ -1,15 +1,11 @@
 cislo = int(input("Zadej číslo: "))
 
-if cislo <= 1:
-    print(f"{cislo} není prvočíslo.")
-else:
-    je_prvocislo = True
+if cislo > 1:
     for i in range(2, int(cislo**0.5) + 1):
         if cislo % i == 0:
-            je_prvocislo = False
+            print(f"{cislo} není prvočíslo.")
             break
-
-    if je_prvocislo:
-        print(f"{cislo} je prvočíslo.")
     else:
-        print(f"{cislo} není prvočíslo.")
+        print(f"{cislo} je prvočíslo.")
+else:
+    print(f"{cislo} není prvočíslo.")
