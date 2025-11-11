ccbd17197b082a2c6636919031d43aba108deb3e
FRIES-roman <24302@student.vitkovickastredni.cz>
2025-11-11 11:13:51 +0100

    delitele_funkce.py
    Refactor delitele function to print divisors directly instead of returning a list.

--- delitele_funkce.py
+++ delitele_funkce.py
@@ -1,11 +1,9 @@
 def delitele(n):
-    delitele_list = []          
-    for i in range(1, n + 1): 
-        if n % i == 0:        
-            delitele_list.append(i)  
-    return delitele_list       
+    print(f"Dělitele čísla {n} jsou:", end=" ")
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i, end=" ")
+    print()  
 
 cislo = int(input("Zadej číslo: "))
-
-vysledek = delitele(cislo)
-print("Dělitele čísla", cislo, "jsou:", vysledek)
+delitele(cislo)
